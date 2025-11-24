165910b56b9b6e99ce38c3bc6d41413fc8056f3f
zakariastrong <114486087+zakariastrong@users.noreply.github.com>
2025-11-24 17:29:38 +0100

    Add some modification

--- scenarios/consult_trips.js
+++ scenarios/consult_trips.js
@@ -1,15 +1,9 @@
-const visit = async (page) => {
+module.exports = async (page) => {
   await page.goto("http://localhost/", {
     waitUntil: "networkidle",
   });
 
-  await page.waitForTimeout(3000);
-
-  await page.evaluate(() => {
-    window.scrollBy(0, 500);
-  });
-
-  await page.waitForNetworkIdle();
-
   await page.waitForTimeout(2000);
+
+  await page.waitForTimeout(3000);
 };
